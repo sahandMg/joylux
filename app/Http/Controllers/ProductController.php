5812158fe9289f3759bd0f0b18403ebfc15f751d
sahandmg <s23.moghadam@gmail.com>
2024-08-22 22:28:20 +0330

    first commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,9 @@ class ProductController extends Controller
     public function getAProduct($code)
     {
         $product = ProductDB::getProductDataBySku($code);
+        if (is_null($product)) {
+            return  response()->json(['error' => 'Not Found'], 404);
+        }
         return response()->json(['data' => $product], 200);
     }
 }
